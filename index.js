578e2cdc18816dd5a5c5d3355dac5f854f9fa2d6
Ильшат Гилязев <GilyazevIM@bashcardio.local>
2024-05-06 16:35:40 +0500

    06.05 W

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ const handlebars = expressHandlebars.create({
 });
 
 import express from 'express';
-import { get } from 'http';
 let app = express();
+let ank = []
+
+import bodyParser from 'body-parser';
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
 app.get('/', (req, res)=>{
-    res.send('hello world');
+    res.send('What do you want here?');
 })
 
-app.get('/page/', function(req, res) {
-	res.render('page', {text1: 'aaa', text2: 'bbb'});
+app.get('/ank/:id', function(req, res) {
+	res.render('ank'+req.params.id);
+});
+
+app.post('/ank_new', (req, res) => {
+	ank.push(req.body);
+	console.log(req.body);
+	res.render('ank_new', {aaa: req.body});
+});
+
+app.get('/get_ank', (req, res) => {
+	console.log('get_ank OKKKK');
+	res.send(ank);
+	ank = [];
 });
 
 app.listen(3000, ()=>{
