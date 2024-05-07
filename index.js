67a873026e4f77f9daf2ffeb4f595efcd0bd5249
Ильшат Гилязев <GilyazevIM@bashcardio.local>
2024-05-07 10:10:20 +0500

    07.05 W

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ let ank = []
 import bodyParser from 'body-parser';
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static('public'))
+
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
 app.get('/', (req, res)=>{
@@ -23,14 +25,14 @@ app.get('/ank/:id', function(req, res) {
 
 app.post('/ank_new', (req, res) => {
 	ank.push(req.body);
-	console.log(req.body);
-	res.render('ank_new', {aaa: req.body});
+	console.log(ank);
+	res.render('ank_new', {fio:req.body.fio});
 });
 
 app.get('/get_ank', (req, res) => {
 	console.log('get_ank OKKKK');
 	res.send(ank);
-	ank = [];
+	//ank = [];
 });
 
 app.listen(3000, ()=>{
